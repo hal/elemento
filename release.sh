1b7414dcd067658260fbac1b79e8c4115a6add09
Harald Pehl <harald.pehl@gmail.com>
2018-09-12 15:36:16 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Script to build, deploy and release Elemento.
 #
 # Prerequisites
@@ -14,6 +13,8 @@
 #   2.
 #   3.
 #   4.
+
+
 
 ROOT=$PWD
 VERSION=$1
@@ -33,11 +34,14 @@ function box()
   tput sgr 0
 }
 
+
+
 # Prerequisites
 if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters. Please use $0 <version>"
     exit -1
 fi
+
 if git rev-parse -q --verify "refs/tags/$VERSION" >/dev/null; then
     echo "A tag for '$VERSION' already exists."
     exit -1
@@ -46,6 +50,7 @@ if git rev-parse -q --verify "refs/tags/$VERSION-gwt2" >/dev/null; then
     echo "A tag for '$VERSION-gwt2' already exists."
     exit -1
 fi
+
 git checkout develop
 if ! git diff --no-ext-diff --quiet --exit-code; then
     echo "Unable to release. You have uncommitted changes in the branch 'develop'."
@@ -57,21 +62,61 @@ if ! git diff --no-ext-diff --quiet --exit-code; then
     exit -1
 fi
 
-# Steps
-box "Switch to branch 'gwt2'"
-git checkout gwt2
-box "Build branch 'gwt2'"
+
+
+# Release branch 'gwt2'
+BRANCH=gwt2
+box "Switch to branch '$BRANCH'"
+git checkout $BRANCH
+git pull origin $BRANCH
+
+box "Build branch '$BRANCH'"
+mvn clean install -P samples,testsuite,widget || { echo "Maven build failed" ; exit 1; }
+
 box "Update version to '$VERSION-gwt2'"
+./versionBump.sh $VERSION-gwt2
+git commit -am "Bump to $VERSION-gwt"
+
 box "Deploy '$VERSION-gwt2'"
-box "Update version to 'GWT2-SNAPSHOT'"
+mvn deploy -P release,samples,testsuite,widget || { echo "Maven deploy failed" ; exit 1; }
+git tag $VERSION-gwt
+git push origin $BRANCH
+git push origin --tags
+
+box "Back to 'GWT2-SNAPSHOT'"
+./versionBump.sh GWT2-SNAPSHOT
+git commit -am "Bump to GWT2-SNAPSHOT"
+git push origin $BRANCH
 
 
-box "Switch to branch 'develop'"
-git checkout develop
-box "Build branch 'develop'"
+
+# Release branch 'develop'
+BRANCH=develop
+box "Switch to branch '$BRANCH'"
+git checkout $BRANCH
+git pull origin $BRANCH
+
+box "Build branch '$BRANCH'"
+mvn clean install -P samples,testsuite,widget || { echo "Maven build failed" ; exit 1; }
+
 box "Update version to '$VERSION'"
+git flow release start $VERSION
+./versionBump.sh $VERSION
+git commit -am "Bump to $VERSION"
+
 box "Deploy '$VERSION'"
-box "Update version to 'HEAD-SNAPSHOT'"
+mvn deploy -P release,samples,testsuite,widget || { echo "Maven deploy failed" ; exit 1; }
+git flow release finish -m "$VERSION" $VERSION
+git push origin develop
+git push origin master
+git push origin --tags
+
+box "Back to 'HEAD-SNAPSHOT'"
+git checkout $BRANCH
+./versionBump.sh HEAD-SNAPSHOT
+git commit -am "Bump to HEAD-SNAPSHOT"
+git push origin $BRANCH
 
 
-box "Elemento successfully released"
+
+box "<---=== Elemento successfully released ===--->"
