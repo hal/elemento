37780071f9616dbc09f1522960cdd45c0963fe6e
Harald Pehl <harald.pehl@gmail.com>
2018-12-02 19:44:16 +0100

    ElementBuilder should no longer implement IsElement

--- core/src/main/java/org/jboss/gwt/elemento/core/builder/ElementBuilder.java
+++ core/src/main/java/org/jboss/gwt/elemento/core/builder/ElementBuilder.java
@@ -45,6 +45,12 @@ public abstract class ElementBuilder<E extends HTMLElement, B extends ElementBui
     }
 
     @Override
+    @Deprecated
+    /**
+     * @deprecated In order to cleanup the API and to avoid multiple methods which do (or return) the same thing,
+     * builders won't implement {@code IsElement<E>} in the next major version. Please use {@link #get()} instead.
+     * A builder is a builder and not an element.
+     */
     public E element() {
         return element;
     }
