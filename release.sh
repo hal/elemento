8dea598f09ffa905ef8896aafdd1553df1dd5334
Harald Pehl <harald.pehl@gmail.com>
2018-12-05 07:58:22 +0100

    Adjust release.sh to exclude samples for branch gwt2

--- release.sh
+++ release.sh
@@ -72,14 +72,14 @@ git checkout $BRANCH
 git pull origin $BRANCH
 
 box "Build branch '$BRANCH'"
-mvn clean install -P samples,widget || { echo "Maven build failed" ; exit 1; }
+mvn clean install -P widget || { echo "Maven build failed" ; exit 1; }
 
 box "Update version to '$VERSION-gwt2'"
 ./versionBump.sh $VERSION-gwt2
 git commit -am "Bump to $VERSION-gwt"
 
 box "Deploy '$VERSION-gwt2'"
-mvn deploy -P release,samples,widget || { echo "Maven deploy failed" ; exit 1; }
+mvn deploy -P release,widget || { echo "Maven deploy failed" ; exit 1; }
 git tag $VERSION-gwt
 git push origin $BRANCH
 git push origin --tags
