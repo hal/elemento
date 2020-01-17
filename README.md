3415996a18f473569c0d254366caf673ca169a00
Harald Pehl <harald.pehl@gmail.com>
2020-01-17 20:08:37 +0100

    Back to HEAD-SNAPSHOT

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Elemento is available in [Maven Central](https://maven-badges.herokuapp.com/mave
 <dependency>
     <groupId>org.elemento</groupId>
     <artifactId>elemento-core</artifactId>
-    <version>1.0.0-rc1</version>
+    <version>HEAD-SNAPSHOT</version>
 </dependency>
 ```
  
--- versionBump.sh
+++ versionBump.sh
@@ -7,7 +7,7 @@ PROGNAME=`basename "$0"`
 if [ "$#" -ne 1 ]; then
     echo "Illegal number of arguments. Use '$PROGNAME <version>'"
 else
-    mvn versions:set -DnewVersion=$1 -P samples
+    mvn versions:set -DnewVersion=$1 -P samples -U
     sed -i.versionsBackup "s/<version>.*<\/version>/<version>$1<\/version>/" README.md
     sed -i.versionsBackup "s/<elemento\.version>.*<\/elemento\.version>/<elemento\.version>$1<\/elemento\.version>/" samples/gwt/pom.xml
     sed -i.versionsBackup "s/<elemento\.version>.*<\/elemento\.version>/<elemento\.version>$1<\/elemento\.version>/" samples/j2cl/pom.xml
