a766ba1657d30578ce1688d6b19fd8ba78d0e401
hpehl <harald.pehl@gmail.com>
2022-03-25 18:07:44 +0100

    Fix GWT.com release script

--- gwtcom.sh
+++ gwtcom.sh
@@ -105,6 +105,6 @@ sed -i '' -f sed/pom.sed pom.xml
 msg "    ${YELLOW}✓${NOFORMAT} Maven POM"
 sed -i '' -f sed/module.sed src/main/resources/org/jboss/elemento/Core.gwt.xml
 msg "    ${YELLOW}✓${NOFORMAT} GWT module"
-sed -i '' -f sed/java.sed src/main/java/org/jboss/elemento/Elements.java
-sed -i '' -f sed/java.sed src/main/java/org/jboss/elemento/EventType.java
+sed -i '' -f sed/import.sed src/main/java/org/jboss/elemento/Elements.java
+sed -i '' -f sed/import.sed src/main/java/org/jboss/elemento/EventType.java
 msg "    ${YELLOW}✓${NOFORMAT} Java imports"
