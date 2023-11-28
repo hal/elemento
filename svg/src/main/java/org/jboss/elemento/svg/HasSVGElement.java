7c350e74bd3f6120f52d40a7517c372f8aaccc11
Harald Pehl <harald.pehl@gmail.com>
2023-11-28 08:17:51 +0100

    Add overloaded style() methods to HasSVGElement

--- svg/src/main/java/org/jboss/elemento/svg/HasSVGElement.java
+++ svg/src/main/java/org/jboss/elemento/svg/HasSVGElement.java
@@ -24,9 +24,40 @@ import org.jboss.elemento.TypedBuilder;
 public interface HasSVGElement<E extends SVGElement, B extends TypedBuilder<E, B>>
         extends TypedBuilder<E, B>, IsElement<E> {
 
-    /** Sets the CSS style of the element. */
+    /** Appends the given style to the existing styles of this element. */
     default B style(String style) {
-        element().style.cssText = style;
+        return style(style, true);
+    }
+
+    /** Appends or sets the given style to the styles of this element. */
+    default B style(String style, boolean append) {
+        if (append) {
+            element().style.cssText += style;
+        } else {
+            element().style.cssText = style;
+        }
+        return that();
+    }
+
+    /** Sets the given style on this element. */
+    default B style(String property, int value) {
+        return style(property, String.valueOf(value), false);
+    }
+
+    /** Sets the given style on this element. */
+    default B style(String property, String value) {
+        return style(property, value, false);
+    }
+
+    /** Sets the given style on this element. */
+    default B style(String property, int value, boolean important) {
+        return style(property, String.valueOf(value), important);
+    }
+
+    /** Sets the given style on this element. */
+    default B style(String property, String value, boolean important) {
+        String priority = important ? "important" : "";
+        element().style.setProperty(property, value, priority);
         return that();
     }
 
