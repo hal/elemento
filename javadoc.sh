5b791e523908f2c3878859eb8cd62b3cef9b7920
Harald Pehl <harald.pehl@gmail.com>
2024-03-11 12:07:31 +0100

    Publish worflow

---
+++ javadoc.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+#
+#  Copyright 2023 Red Hat
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+
+# --------------------------------------------------
+#
+# Generating aggregate javadoc with code snippets is PITA!
+# As a workaround this script generates javadoc for each
+# module and manually aggregates the documentation
+#
+# --------------------------------------------------
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+VERSION=0.0.1
+
+# Change into the script's directory
+# Using relative paths is safe!
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+readonly script_dir
+cd "${script_dir}"
+
+usage() {
+  cat <<EOF
+USAGE:
+    $(basename "${BASH_SOURCE[0]}") [FLAGS] <modules>
+
+FLAGS:
+    -h, --help          Prints help information
+    -v, --version       Prints version information
+    --no-color          Uses plain text output
+
+ARGS:
+    <modules>           The modules for which javadoc is to be generated
+EOF
+  exit
+}
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+}
+
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    # shellcheck disable=SC2034
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+version() {
+  msg "${BASH_SOURCE[0]} $VERSION"
+  exit 0
+}
+
+parse_params() {
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --version) version ;;
+    --no-color) NO_COLOR=1 ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+  return 0
+}
+
+parse_params "$@"
+setup_colors
+
+MODULES=("flow" "mathml" "router" "svg")
+PACKAGE_DIR="org/jboss/elemento"
+SITE_API_DOCS="target/site/apidocs"
+
+./mvnw --show-version --batch-mode clean
+./mvnw --show-version --batch-mode -P snippets javadoc:aggregate
+./mvnw --show-version --batch-mode -P snippets javadoc:javadoc
+
+mkdir -p "$SITE_API_DOCS/$PACKAGE_DIR"
+find "core/$SITE_API_DOCS/$PACKAGE_DIR" -type f -name "*.html" -maxdepth 1 \
+    -exec cp -v {} "$SITE_API_DOCS/$PACKAGE_DIR" \;
+for module in ${MODULES[@]}; do
+    mkdir -p "$SITE_API_DOCS/$PACKAGE_DIR/$module"
+    find "$module/$SITE_API_DOCS/$PACKAGE_DIR/$module" -type f -name "*.html" -maxdepth 1 \
+        -exec cp -v {} "$SITE_API_DOCS/$PACKAGE_DIR/$module" \;
+done
