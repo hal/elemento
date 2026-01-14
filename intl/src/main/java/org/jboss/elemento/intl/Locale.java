a3650e61ded60ee52b6f1113298a064c0a1b5e9f
Harald Pehl <harald.pehl@gmail.com>
2026-01-14 19:38:12 +0100

    Enhance API documentation

---
+++ intl/src/main/java/org/jboss/elemento/intl/Locale.java
@@ -0,0 +1,53 @@
+/*
+ *  Copyright 2023 Red Hat
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.jboss.elemento.intl;
+
+import elemental2.core.JsArray;
+import jsinterop.annotations.JsType;
+import jsinterop.base.JsPropertyMap;
+
+import static jsinterop.annotations.JsPackage.GLOBAL;
+
+/**
+ * Mapping for <a
+ * href="https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale"><code>Intl.Locale</code></a>.
+ *
+ * @see <a
+ * href="https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale">https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale</a>
+ */
+@SuppressWarnings("unused")
+@JsType(isNative = true, namespace = GLOBAL, name = "Intl.Locale")
+public class Locale {
+
+    // @formatter:off
+    public Locale(String locale) {}
+    public Locale(String locale, LocaleOptions options) {}
+
+    public native String format(JsArray<String> list);
+    public native JsArray<JsPropertyMap<String>> formatToParts(JsArray<String> list);
+    public native JsPropertyMap<String> resolvedOptions();
+
+    public native JsArray<String> getCalendars();
+    public native JsArray<String> getCollations();
+    public native JsArray<String> getHourCycles();
+    public native JsArray<String> getNumberingSystems();
+    public native TextInfo getTextInfo();
+    public native JsArray<String> getTimeZones();
+    public native WeekInfo getWeekInfo();
+    public native Locale maximize();
+    public native Locale minimize();
+    // @formatter:on
+}
---
+++ intl/src/main/java/org/jboss/elemento/intl/LocaleOptions.java
@@ -0,0 +1,119 @@
+/*
+ *  Copyright 2023 Red Hat
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.jboss.elemento.intl;
+
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import jsinterop.base.Js;
+import jsinterop.base.JsPropertyMap;
+
+import static jsinterop.annotations.JsPackage.GLOBAL;
+
+/**
+ * Mapping for <a
+ * href="https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale/Locale#options">locale
+ * options</a> passed to {@link Locale} constructors.
+ * <p>
+ * In addition to the actual mappings, this class implements the builder pattern using enums to create the options easily and in
+ * a type-safe manner. Some enums are shared with other format option builders of the <code>Intl</code> module and may contain
+ * more values than intended for the given method. In that case the allowed values are listed on the method. In all other cases
+ * all enum values are applicable.
+ *
+ * @see <a
+ * href="https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale/Locale#options">https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale/Locale#options</a>
+ */
+@SuppressWarnings("unused")
+@JsType(isNative = true, namespace = GLOBAL)
+public interface LocaleOptions {
+
+    // @formatter:off
+    @JsOverlay static LocaleOptions create() { return Js.uncheckedCast(JsPropertyMap.of()); }
+    @JsProperty void setLanguage(String language);
+    @JsProperty void setScript(String script);
+    @JsProperty void setRegion(String region);
+    @JsProperty void setVariants(String variants);
+    @JsProperty void setCalendar(String calendar);
+    @JsProperty void setCollation(String collation);
+    @JsProperty void setNumberingSystem(String numberingSystem);
+    @JsProperty void setCaseFirst(String caseFirst);
+    @JsProperty void setHourCycle(String hourCycle);
+    @JsProperty void setNumeric(boolean numeric);
+    // @formatter:on
+
+    // ------------------------------------------------------ builder
+
+    @JsOverlay
+    default LocaleOptions language(String language) {
+        setLanguage(language);
+        return this;
+    }
+
+    @JsOverlay
+    default LocaleOptions script(String script) {
+        setScript(script);
+        return this;
+    }
+
+    @JsOverlay
+    default LocaleOptions region(String region) {
+        setRegion(region);
+        return this;
+    }
+
+    @JsOverlay
+    default LocaleOptions variants(String variants) {
+        setVariants(variants);
+        return this;
+    }
+
+    @JsOverlay
+    default LocaleOptions calendar(String calendar) {
+        setCalendar(calendar);
+        return this;
+    }
+
+    @JsOverlay
+    default LocaleOptions collation(String collation) {
+        setCollation(collation);
+        return this;
+    }
+
+    @JsOverlay
+    default LocaleOptions numberingSystem(String numberingSystem) {
+        setNumberingSystem(numberingSystem);
+        return this;
+    }
+
+    @JsOverlay
+    default LocaleOptions caseFirst(CaseFirst caseFirst) {
+        setCaseFirst(caseFirst.value);
+        return this;
+    }
+
+    /** Allowed values: {@link HourCycle#h23}, {@link HourCycle#h12}, and {@link HourCycle#h11}. */
+    @JsOverlay
+    default LocaleOptions hourCycle(HourCycle hourCycle) {
+        setHourCycle(hourCycle.value);
+        return this;
+    }
+
+    @JsOverlay
+    default LocaleOptions numeric(boolean numeric) {
+        setNumeric(numeric);
+        return this;
+    }
+}
